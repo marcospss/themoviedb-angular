9bc267de1db87bedf36acea9f2f5b21fccbcbf81
marcospss <marcospss@gmail.com>
2021-05-24 15:40:49 -0300

    Convert app for PWA

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -6,6 +6,8 @@ import { RootComponent } from './root.component';
 
 import { SharedModule } from '@application/shared/shared.module';
 import { InfrastructureModule } from '@infrastructure/infrastructure.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '@infrastructure/environments/environment';
 @NgModule({
   declarations: [RootComponent],
   imports: [
@@ -13,6 +15,12 @@ import { InfrastructureModule } from '@infrastructure/infrastructure.module';
     AppRoutingModule,
     SharedModule,
     InfrastructureModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
   bootstrap: [RootComponent],
